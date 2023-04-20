f95f78d51492cad86097ed1c3c50a42f25438545
Haomiao Jiang <hjiang36@gmail.com>
2023-04-20 15:04:08 -0700

    Update requirements.txt and gitignore file

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from util.render import GUIRenderer
 from util.data_structs import ImageDataStructs
 
 if __name__ == "__main__":
-    renderer = GUIRenderer(1280, 720, "ID Photo Editor")
+    renderer = GUIRenderer(960, 640, "ID Photo Editor")
     data = ImageDataStructs()
     
     while not renderer.should_close():
